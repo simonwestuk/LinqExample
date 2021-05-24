7144770c27dfcf4baa10cb623d1381df075e68fe
Simon West <43855704+simonwestuk@users.noreply.github.com>
2021-05-24 10:24:42 +0100

    Add project files.

---
+++ LinqExample/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqExample
+{
+    class Customer
+    {
+        public Guid Id { get; set; }
+        public string Fname { get; set; }
+        public string Sname { get; set; }
+        public string Email { get; set; }
+        public DateTime Dob { get; set; }
+        public string Tel { get; set; }
+
+        public Customer(string fname, string sname, string email, DateTime dob, string tel)
+        {
+            Id = Guid.NewGuid();
+            Fname = fname;
+            Sname = sname;
+            Email = email;
+            Dob = dob;
+            Tel = tel;
+        }
+
+        public override string ToString()
+        {
+            return $@"
+            ID: {Id}
+            Name: {Fname} {Sname}
+            Email: {Email}
+            DOB: {Dob}
+            Tel: {Tel}";
+
+        }
+    }
+}
---
+++ LinqExample/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Customer> Customers = new List<Customer>();
+
+            Customers.Add(new Customer("Simon", "West", "simon@gmail.com", new DateTime(1986, 7, 30), "0123456789"));
+
+            Customers.Add(new Customer("Sally", "Wilkinson", "sally@gmail.com", new DateTime(1998, 2, 3), "0987654321"));
+
+            Customers.Add(new Customer("John", "Smith", "jsmith@gmail.com", new DateTime(1968, 5, 4), "07654321882"));
+
+
+            //LINQ Query
+            try
+            {
+                Console.WriteLine("Enter search term:");
+                int term = Convert.ToInt32(Console.ReadLine());
+
+                foreach (var customer in Customers.Where(c => c.Dob.Year == term))
+                {
+                    Console.WriteLine(customer);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Needs to be a number.");
+            }
+
+            //If Statement
+            try
+            {
+                Console.WriteLine("Enter search term:");
+                int term = Convert.ToInt32(Console.ReadLine());
+
+                foreach (var customer in Customers)
+                {
+                    if (customer.Dob.Year == term)
+                    {
+                        Console.WriteLine(customer);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Needs to be a number.");
+            }
+
+            Console.WriteLine("-------------------------------------------------------");
+            //Starts with S, convert ToUpper to ensure we do not miss any name which may start with 's'
+            foreach (var customer in Customers.Where(c => c.Fname.ToUpper().StartsWith("S")))
+            {
+                Console.WriteLine(customer);
+            }
+
+        }
+    }
+}
